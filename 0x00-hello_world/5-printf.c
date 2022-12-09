cffff27ca3bfe534bae0c7ec246cc47a8d282b54
simon jerry <swagasoft@gmail.com>
2022-12-09 10:20:00 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * decription - A C program that print out using printf finction
+ *
+ * return 0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+}
