85bcd06abbd910c10845feebc7b757ba90a3501c
simon jerry <swagasoft@gmail.com>
2022-12-10 06:59:19 -0800

    print Dora

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main: Entry main method
+ *
+ * Return: Always 0 for success
+  */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora \
+			Korper, 2015-10-19\n");
+	return (1);
+}
