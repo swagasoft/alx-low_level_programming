3898d737d7f5b891730400e559758502e80ac93e
simon jerry <swagasoft@gmail.com>
2022-12-10 07:10:36 -0800

    added description

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 /**
- * main: Entry main method
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
- * Return: Always 0 for success
-  */
+ * Return: Always 1.
+ */
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora \
-			Korper, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n");
 	return (1);
 }
