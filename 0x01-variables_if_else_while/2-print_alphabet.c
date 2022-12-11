64f50a4cc0090087aefe6d87cd8d7b1d5072ed9a
simon jerry <swagasoft@gmail.com>
2022-12-11 11:46:16 -0800

    convert to lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		++ch;
+	}
+	putchar('\n');
+
+	return (0);
+}
