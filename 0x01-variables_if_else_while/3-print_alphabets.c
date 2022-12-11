bcd0fbb3e247880e2d743a76b04f4f4d1adbc30b
simon jerry <swagasoft@gmail.com>
2022-12-11 11:48:09 -0800

    print a to z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet in lowercase then in uppercase
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	/*prints a - z*/
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		++ch;
+	}
+
+	/*print A - Z*/
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		++CH;
+	}
+	putchar('\n');
+
+	return (0);
+}
