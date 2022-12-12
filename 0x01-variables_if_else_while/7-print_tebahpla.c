da09322f8f6188d9c71c64782354dd2a9793314f
simon jerry <swagasoft@gmail.com>
2022-12-12 05:09:56 -0800

    print lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a-z in reverse
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		--ch;
+	}
+	putchar('\n');
+
+	return (0);
+}
