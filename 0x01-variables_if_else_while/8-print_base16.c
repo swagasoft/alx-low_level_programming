48d359779f0e9c929c24107ca007b7eb9ba2b141
simon jerry <swagasoft@gmail.com>
2022-12-12 05:22:58 -0800

    print base 16 num in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,19 +3,23 @@
 /**
  * main - Entry point
  *
- * Description: print a-z in reverse
+ * Description: print numbers of base16 in lowercase
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	char cha = 'z';
+	int num = 48; /*48; decimal rep of 0*/
 
-	while (cha >= 'a')
+	while (num <= 102) /*102; decimal rep of f*/
 	{
-		putchar(cha);
-		--cha;
+		putchar(num);
+
+		/* after 9 we jump till 96; `*/
+		if (num == 57)
+			num += 39;
+		++num;
 	}
 	putchar('\n');
 
