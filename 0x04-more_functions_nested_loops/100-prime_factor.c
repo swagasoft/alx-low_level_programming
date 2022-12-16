31a2b320f75696b0be7a4cfcbcbdfdf84e568bf2
simon jerry <swagasoft@gmail.com>
2022-12-16 02:16:04 -0800

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -  calculate largest prime of 612852475143
+ *
+ * Return: (0) Success 
+ */
+
+int main(void)
+{
+	long int num, prime;
+
+	num = 612852475143;
+	for (prime = 2; prime <= num; prime++)
+	{
+		if (num % prime == 0)
+		{
+			num /= prime;
+			prime--;
+		}
+	}
+	printf("%ld\n", prime);
+	return (0);
+}
