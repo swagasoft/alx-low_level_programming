82e90fede95d365b800f614acf69b349f7dbf70e
simon jerry <swagasoft@gmail.com>
2022-12-22 11:02:41 -0800

    concat string

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+void print_rev(char *s);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+
+#endif
