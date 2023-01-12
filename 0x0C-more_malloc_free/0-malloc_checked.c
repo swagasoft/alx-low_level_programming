637e122631556e988b38a6d41e340906db92fc73
simon jerry <swagasoft@gmail.com>
2023-01-12 06:19:00 -0800

    resolve betty error

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include "main.h"
+
 /**
-* malloc_checked - 
-* @b: bytes
-* Return:pointer
-*/
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
 
 void *malloc_checked(unsigned int b)
 {
