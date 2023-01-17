ece33cfd8085c64153eebeb3af71ae0c2483c879
simon jerry <swagasoft@gmail.com>
2023-01-17 12:18:11 -0800

    print previous name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: Everything worked ok
+ */
+
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
