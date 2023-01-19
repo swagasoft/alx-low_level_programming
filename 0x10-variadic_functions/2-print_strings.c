8f4de7a5e804b95a7190b334560aa0435faf8ab3
simon jerry <swagasoft@gmail.com>
2023-01-19 12:43:30 -0800

    print a string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,12 +1,28 @@
 #include "variadic_functions.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_strings - Print numbers using a separator
+ * @separator: The string separator
+ * @n: The numbers
+ * @...: The list
+ * Return: Nothing
  */
-int main(void)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-    print_strings(", ", 2, "Jay", "Django");
-    return (0);
+	va_list strings;
+	unsigned int i;
+	char *p;
+
+	va_start(strings, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (separator != NULL && i != 0)
+			printf("%s", separator);
+		p = va_arg(strings, char *);
+			printf("%s", (p == NULL) ? "(nil)" : p);
+	}
+	printf("\n");
+	va_end(strings);
 }
